022877c36e4382de551af6f00c0e0e7afb1d0b30
Mateus Andrade <mat.andrade@live.com>
2020-05-01 13:20:33 -0300

    added bcrypts

--- module02/api/src/services/CreateUserService.ts
+++ module02/api/src/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
 
 import User from '../models/User';
 
@@ -20,13 +21,17 @@ export default class CreateUserService {
       throw new Error('Email address already used');
     }
 
+    const hashedPassword = await hash(password, 10);
+
     const user = userRepository.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     await userRepository.save(user);
+
+    delete user.password;
 
     return user;
   }
