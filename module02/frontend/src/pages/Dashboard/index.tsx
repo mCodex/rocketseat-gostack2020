0c29feb2c0b2c3e9413b2172aae00d2c801e43cb
Mateus Andrade <mat.andrade@live.com>
2020-05-03 12:06:19 -0300

    loading data from github API

--- module02/frontend/src/pages/Dashboard/index.tsx
+++ module02/frontend/src/pages/Dashboard/index.tsx
@@ -1,35 +1,66 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
+import api from '../../services/api';
+
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  const handleAddRepository = async (
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> => {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    setRepositories([...repositories, response.data]);
+    setNewRepo('');
+  };
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
 
-      <Form>
+      <Form onSubmit={handleAddRepository}>
         <input
           type="text"
-          name="repositoryName"
+          value={newRepo}
+          name="newRepo"
+          onChange={(e) => setNewRepo(e.target.value)}
           placeholder="Digite o nome do repositório"
         />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a>
-          <img src="" alt="" />
-          <div>
-            <strong>Teste</strong>
-            <p>Teste</p>
-          </div>
+        {repositories.map((repository) => (
+          <a key={repository.full_name}>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
--- module02/frontend/src/pages/Dashboard/styles.ts
+++ module02/frontend/src/pages/Dashboard/styles.ts
@@ -74,7 +74,8 @@ export const Repositories = styled.div`
     }
 
     div {
-      margin-left: 20px;
+      margin-left: 16px;
+      flex: 1;
 
       strong {
         font-size: 20px;
