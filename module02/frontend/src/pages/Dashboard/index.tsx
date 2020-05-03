0b818b072e0e549a7932222530745720e844aa3d
Mateus Andrade <mat.andrade@live.com>
2020-05-03 11:24:56 -0300

    working on dashboard screen

--- module02/frontend/src/pages/Dashboard/index.tsx
+++ module02/frontend/src/pages/Dashboard/index.tsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input
+          type="text"
+          name="repositoryName"
+          placeholder="Digite o nome do repositório"
+        />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a>
+          <img src="" alt="" />
+          <div>
+            <strong>Teste</strong>
+            <p>Teste</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
