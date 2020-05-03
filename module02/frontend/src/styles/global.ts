73c63acd0c70cc8cdeff32e02f50f89efc496e16
Mateus Andrade <mat.andrade@live.com>
2020-05-03 10:56:45 -0300

    added global style

---
+++ module02/frontend/src/styles/global.ts
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from 'styled-components';
+
+import bg from '../assets/bg.svg';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #F0F0F5 url(${bg}) no-repeat 70% top;
+    -webkit-font-smoothing: antialised;
+  }
+
+  body, input, button {
+    font: 16px Lato, sans-serif;
+  }
+
+  #root {
+    max-width: 960px;
+    margin: 0 auto;
+    padding: 40px 20px;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
