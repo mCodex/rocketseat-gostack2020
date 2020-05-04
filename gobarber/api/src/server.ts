1ea4189c83cb47bc5769ddb40e56bc7d35fc80f5
Mateus Andrade <mat.andrade@live.com>
2020-05-04 18:17:33 -0300

    added cors & helmet in gobarber API

--- gobarber/api/src/server.ts
+++ gobarber/api/src/server.ts
@@ -2,6 +2,8 @@ import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
+import helmet from 'helmet';
 
 import AppError from './errors/AppError';
 
@@ -14,6 +16,8 @@ import './database';
 const app = express();
 const port = process.env.PORT || 3333;
 
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
 
 app.use('/files', express.static(uploadConfig.directory));
