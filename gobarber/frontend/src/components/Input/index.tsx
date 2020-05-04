1d9e0d7a49ced2b9df563a2c4b2a3b64e7586f2d
Mateus Andrade <mat.andrade@live.com>
2020-05-04 16:44:16 -0300

    added form validation

--- gobarber/frontend/src/components/Input/index.tsx
+++ gobarber/frontend/src/components/Input/index.tsx
@@ -50,6 +50,7 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
         onBlur={handleInputBlur}
         {...rest}
       />
+      {error && error}
     </Container>
   );
 };
