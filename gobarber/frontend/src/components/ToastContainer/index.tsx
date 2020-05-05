afceada654817fb188142bac058391f5d003574b
Mateus Andrade <mat.andrade@live.com>
2020-05-05 19:36:53 -0300

    working on toast

--- gobarber/frontend/src/components/ToastContainer/index.tsx
+++ gobarber/frontend/src/components/ToastContainer/index.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
 
-import { Container, Toast } from './styles';
+import { ToastMessage, useToast } from '../../hooks/toast';
 
-const ToastContainer: React.FC = () => {
+import Toast from './Toast';
+
+import { Container } from './styles';
+
+interface ToasContainerProps {
+  messages: ToastMessage[];
+}
+
+const ToastContainer: React.FC<ToasContainerProps> = ({ messages }) => {
+  const { removeToast } = useToast();
+
   return (
     <Container>
-      <Toast type="info" hasDescription={false}>
-        <FiAlertCircle />
-
-        <div>
-          <strong>Aconteceu um erro</strong>
-          {/* <p>Não foi possível fazer login na aplicação</p> */}
-        </div>
-
-        <button type="button">
-          <FiXCircle size={18} />
-        </button>
-      </Toast>
+      {messages.map((message) => (
+        <Toast key={message.id} message={message} />
+      ))}
     </Container>
   );
 };
