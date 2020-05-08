84bb11aa0f4bc132c088c81d84143276f025043e
Mateus Andrade <mat.andrade@live.com>
2020-05-08 18:00:42 -0300

    added create appointment test & jest config

--- gobarber/api/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ gobarber/api/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,22 @@
-test('sum two numbers', () => {
-  expect(2 + 4).toBe(6);
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '1',
+    });
+
+    expect(appointment).toHaveProperty('id');
+  });
+
+  // it('should not be able to create two appointments on the same time', () => {
+
+  // })
 });
