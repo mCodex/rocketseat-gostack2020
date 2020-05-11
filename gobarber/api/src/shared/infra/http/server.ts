adec87c5c6439fd04c8d47f8bd7e440f3defb1e0
Mateus Andrade <mat.andrade@live.com>
2020-05-11 20:29:06 -0300

    update de libs

--- gobarber/api/src/shared/infra/http/server.ts
+++ gobarber/api/src/shared/infra/http/server.ts
@@ -20,7 +20,7 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.use('/files', express.static(uploadConfig.directory));
+app.use('/files', express.static(uploadConfig.tmpFolder));
 app.use(routes);
 
 app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
