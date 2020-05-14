709331f7850627d087f276fb36d0a50666c9514d
Mateus Andrade <mat.andrade@live.com>
2020-05-14 20:32:18 -0300

    added ListProviderMonthAvailability logic

--- gobarber/api/src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
+++ gobarber/api/src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -17,12 +17,12 @@ describe('ListProviderMonthAvailability', () => {
   it('should be able to list the month availability from provider', async () => {
     await fakeAppointmentsRepository.create({
       provider_id: 'user_one',
-      date: new Date(2020, 3, 20, 8, 0, 0),
+      date: new Date(2020, 4, 20, 8, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user_one',
-      date: new Date(2020, 4, 20, 8, 0, 0),
+      date: new Date(2020, 4, 20, 9, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
@@ -32,7 +32,37 @@ describe('ListProviderMonthAvailability', () => {
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user_one',
-      date: new Date(2020, 4, 21, 10, 0, 0),
+      date: new Date(2020, 4, 20, 11, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user_one',
+      date: new Date(2020, 4, 20, 12, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user_one',
+      date: new Date(2020, 4, 20, 13, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user_one',
+      date: new Date(2020, 4, 20, 14, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user_one',
+      date: new Date(2020, 4, 20, 15, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user_one',
+      date: new Date(2020, 4, 20, 16, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user_one',
+      date: new Date(2020, 4, 20, 17, 0, 0),
     });
 
     const availability = await listProviderMonthAvailabilityService.execute({
@@ -53,7 +83,7 @@ describe('ListProviderMonthAvailability', () => {
         },
         {
           day: 21,
-          available: false,
+          available: true,
         },
         {
           day: 22,
