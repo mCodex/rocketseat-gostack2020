1b0a9576494ee4a2841c61720185465e35018401
Mateus Andrade <mat.andrade@live.com>
2020-05-16 16:59:54 -0300

    caching providers list

--- gobarber/api/src/modules/users/services/CreateUserService.ts
+++ gobarber/api/src/modules/users/services/CreateUserService.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import User from '../infra/typeorm/entities/User';
 
 import IUsersRepository from '../repositories/IUsersRepository';
@@ -21,6 +22,9 @@ export default class CreateUserService {
     private usersRepository: IUsersRepository,
     @inject('HashProvider')
     private hashProvider: IHashProvider,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   async execute({ name, email, password }: IRequest): Promise<User> {
@@ -37,6 +41,8 @@ export default class CreateUserService {
       email,
       password: hashedPassword,
     });
+
+    await this.cacheProvider.invalidatePrefix('providers-list');
 
     return user;
   }
