23a9752935487731ce4c0fa0a74be33711eb280b
Mateus Andrade <mat.andrade@live.com>
2020-05-16 16:40:43 -0300

    added redis & caching some endpoints

--- gobarber/api/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ gobarber/api/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,7 +1,7 @@
 import { injectable, inject } from 'tsyringe';
-import { getDaysInMonth, getDate } from 'date-fns';
 
 import AppError from '@shared/errors/AppError';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import Appointment from '../infra/typeorm/entities/Appointment';
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
@@ -23,6 +23,8 @@ class ListProviderAppointmentsService {
   constructor(
     @inject('AppointmentsRepository')
     private appointmentsRepository: IAppointmentsRepository,
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({
@@ -31,6 +33,8 @@ class ListProviderAppointmentsService {
     month,
     year,
   }: IRequest): Promise<Appointment[]> {
+    // const cachedData = await this.cacheProvider.recover('asd');
+
     const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
       {
         provider_id,
@@ -39,6 +43,8 @@ class ListProviderAppointmentsService {
         year,
       },
     );
+
+    // await this.cacheProvider.save('asd', 'asd');
 
     return appointments;
   }
--- gobarber/api/src/modules/appointments/services/ListProvidersService.ts
+++ gobarber/api/src/modules/appointments/services/ListProvidersService.ts
@@ -1,9 +1,8 @@
 import { injectable, inject } from 'tsyringe';
 
-import AppError from '@shared/errors/AppError';
-
 import User from '@modules/users/infra/typeorm/entities/User';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 interface IRequest {
   user_id: string;
@@ -14,12 +13,25 @@ class ListProvidersService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({ user_id }: IRequest): Promise<User[]> {
-    const users = await this.usersRepository.findAllProviders({
+    let users = await this.cacheProvider.recover<User[]>(
+      `providers-list:${user_id}`,
+    );
+
+    if (users) {
+      return users;
+    }
+
+    users = await this.usersRepository.findAllProviders({
       except_user_id: user_id,
     });
+
+    await this.cacheProvider.save(`providers-list:${user_id}`, users);
 
     return users;
   }
