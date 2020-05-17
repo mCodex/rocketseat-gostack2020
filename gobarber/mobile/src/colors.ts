8463218399afd119cd6d48587b4e11036667568b
Mateus Andrade <mat.andrade@live.com>
2020-05-17 11:11:21 -0300

    added icons & changing Input's and Button's styles

--- gobarber/mobile/src/colors.ts
+++ gobarber/mobile/src/colors.ts
@@ -1,7 +1,9 @@
 interface IColors {
   black: string;
+  orange: string;
 }
 
 export default {
   black: '#312e38',
+  orange: '#ff9000',
 } as IColors;
--- gobarber/mobile/src/pages/SignIn/index.tsx
+++ gobarber/mobile/src/pages/SignIn/index.tsx
@@ -3,12 +3,21 @@ import { Image } from 'react-native';
 
 import logoImg from '../../assets/logo.png';
 
-import { Container } from './styles';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
+      <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button>Entrar</Button>
     </Container>
   );
 };
