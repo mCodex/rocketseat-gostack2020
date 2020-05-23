d405a5d3e11afdd239edc813fd26f0e7c2f011d3
Mateus Andrade <mat.andrade@live.com>
2020-05-23 12:10:20 -0300

    added next appointment component inside dashboard

--- gobarber/frontend/src/pages/Dashboard/index.tsx
+++ gobarber/frontend/src/pages/Dashboard/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
-import { FiPower } from 'react-icons/fi';
+import { FiPower, FiClock } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
 
-import { Container, Header, HeaderContent, Profile } from './styles';
 import { useAuth } from '../../hooks/auth';
+
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Calendar,
+} from './styles';
 
 const Dashboard: React.FC = () => {
   const { signOut, user } = useAuth();
@@ -27,6 +37,32 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img src={user.avatar_url} alt="Mateus" />
+
+              <strong>Mateus Andrade</strong>
+
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
